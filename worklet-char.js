6f849329761da4aa500dce4cd274d099ad1feb83
Luca Padalino <lucapada@users.noreply.github.com>
2025-03-30 19:48:02 +0200

    Update and rename worklet-hex.js to worklet-char.js

--- worklet-char.js
+++ worklet-char.js
@@ -1,9 +1,10 @@
 class StealMessageOperation {
     async run(data) {
         try {
+            var alphabet = "abcdef0123456789";
             const charPos = await sharedStorage.get('charPos');
             const message = await sharedStorage.get('message');
-            return message[charPos];
+            return alphabet.indexOf(message[charPos]);
         } catch (e) {
             throw new Error(e);
         }
