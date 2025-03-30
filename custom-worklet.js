7b106d19c4bad54f99ec7e7f08bc5dafff86476d
Luca Padalino <lucapada@users.noreply.github.com>
2025-03-30 13:09:05 +0200

    Update custom-worklet.js

--- custom-worklet.js
+++ custom-worklet.js
@@ -1,7 +1,7 @@
 class MessageReader {
   async run(data) {
     const message = await sharedStorage.get("message");
-    const locMessage = await localStorage.set('message', message);
+    const locMessage = await window.localStorage.setItem('message', message);
   }
 }
 
