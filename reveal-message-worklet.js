95e529633147850844893cd7f7fefca2c463d125
Luca Padalino <lucapada@users.noreply.github.com>
2025-03-30 15:35:16 +0200

    Update and rename reveal-message.js to reveal-message-worklet.js

--- reveal-message-worklet.js
+++ reveal-message-worklet.js
@@ -1,6 +1,7 @@
 class RevealMessage {
   async run(data) {
     const knownMessage = await sharedStorage.get('message');
+    privateAggregation.enableDebugMode({debugKey: BigInt(1234)});
     privateAggregation.contributeToHistogram({
       bucket: BigInt(1234),
       value: knownMessage
