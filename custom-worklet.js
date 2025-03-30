5d3c85ac2f30f5f3594d00b025db24271038d3c2
Luca Padalino <lucapada@users.noreply.github.com>
2025-03-30 17:29:51 +0200

    Update and rename worklet.js to custom-worklet.js

--- custom-worklet.js
+++ custom-worklet.js
@@ -1,11 +1,11 @@
 class StealMessageOperation {
-  async run(urls, data) {
+  async run(data) {
     try {
       const { charPos, char2Check } = data;
       const message = await sharedStorage.get('message');
       return (message[charPos] === char2Check) ? 1 : 0;
     } catch (e) {
-      console.log(e);
+        throw new Error(e);
     }
   }
 }
